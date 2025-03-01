a55161897bfbb5b78388ec764d7a7995ee0bfe13
HAHOOS <hahoos@hahoos.pl>
2025-03-01 10:44:50 +0100

    Make FPS public

--- LabPresence/Helper/FPS.cs
+++ LabPresence/Helper/FPS.cs
@@ -2,9 +2,15 @@
 
 namespace LabPresence.Helper
 {
-    internal static class FPS
+    /// <summary>
+    /// Class responsible for checking the Frames Per Second of the game
+    /// </summary>
+    public static class FPS
     {
-        internal static int FramesPerSecond { get; private set; }
+        /// <summary>
+        /// Frames per second value
+        /// </summary>
+        public static int FramesPerSecond { get; private set; }
 
         internal static int FramesRendered { get; private set; }
 
