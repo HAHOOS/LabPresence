bfcaf5ed823f1a81e71701315c4ad96ad2641e7c
HAHOOS <hahoos@hahoos.dev>
2026-07-01 12:16:57 +0200

    chore: simplify FPS code

--- LabPresence/Utilities/FPS.cs
+++ LabPresence/Utilities/FPS.cs
@@ -8,23 +8,7 @@ namespace LabPresence.Utilities
     {
         public static int FramesPerSecond { get; private set; }
 
-        internal static int FramesRendered { get; private set; }
-
-        internal static DateTime LastTime { get; private set; }
-
-        private static float updateTime;
-
         internal static void OnUpdate()
-        {
-            FramesRendered++;
-            updateTime += Time.deltaTime;
-            if (updateTime >= 1)
-            {
-                updateTime = 0;
-                FramesPerSecond = FramesRendered;
-                FramesRendered = 0;
-                LastTime = DateTime.Now;
-            }
-        }
+            => FramesPerSecond = (int)(1f / Time.unscaledDeltaTime);
     }
 }
